35eb872c6757b3283ecd304954671f24c8c6ac5b
Olin <olin.goog@gmail.com>
2026-02-26 16:34:25 -0500

    new eyetrackingmanager werks

--- Assets/Scripts/Data Collection/EyeTrackingManager.cs
+++ Assets/Scripts/Data Collection/EyeTrackingManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEditor;
 using System;
 using System.IO;
 using System.Runtime.InteropServices;
@@ -8,6 +9,24 @@ using System.Collections.Generic;
 using UnityEngine.UI;
 using System.Diagnostics;
 using System.Net.Http.Headers;
+
+public class Calibrator : EditorWindow
+{
+    [MenuItem("Calibration/StartCalibration")]
+    private static void StartCalibration()
+    {
+        if (EyeTrackingManager.instance != null)
+        {
+            // Call the non-static method on the singleton instance
+            EyeTrackingManager.instance.PupilCalibration();
+        }
+        else
+        {
+            UnityEngine.Debug.LogError("EyeTrackingManager instance is not available.");
+        }
+
+    }
+}
 
 namespace ViveSR.anipal.Eye
 {
@@ -26,11 +45,14 @@ namespace ViveSR.anipal.Eye
         private const int logEveryN = 10;
         private static string logPath;
         private static StreamWriter writer;
+        private static StreamWriter incrementsWriter;
         private static Queue<string> logQueue = new Queue<string>();
+        private static Queue<string> incQueue = new Queue<string>();
 
         // Luminance stuff
         private bool luminanceEnabled = true;
         private bool luminanceCreated = false;
+        private bool callbackReceived = false;
         private int luminanceWidth = 256;
         private int luminanceHeight = 144;
         private Camera luminanceCamera;
@@ -43,32 +65,31 @@ namespace ViveSR.anipal.Eye
         private Camera cam;
         public float fearPeriod = 30.0f; //seconds
         private static int fearStart = -1;
-        private static List<float> fearIncrements;
+        private static List<float> increments;
         private static bool fearChecked = false;
-        private static bool fearEnabled = true; //??
-
+        public bool CalibrateOnStart = true;
+        private bool calibrated = false;
+        private bool calibrating = false;
+        string logDirectory;
         internal class MonoPInvokeCallbackAttribute : System.Attribute
         {
             public MonoPInvokeCallbackAttribute() { }
         }
 
+        public bool isCalibrated()
+        {
+            return calibrated;
+        }
         void Start()
         {
             if (instance == null)
-            {
                 instance = this;
-            }
-
 
             if (!SRanipal_Eye_Framework.Instance.EnableEye)
-            {
                 return;
-            }
-            //float waitTimeBeforeEyeDataCallbackStarted = 3.0f;
-            //yield return new WaitForSecondsRealtime(waitTimeBeforeEyeDataCallbackStarted);
+
             SRanipal_Eye_Framework.Instance.EnableEyeDataCallback = true;
 
-            // Find subject name, the camera is named after the subject
             Camera cameraComponent = UnityEngine.Object.FindObjectOfType<Camera>();
 
             if (cameraComponent != null)
@@ -84,16 +105,12 @@ namespace ViveSR.anipal.Eye
                 return;
             }
 
-            string sceneName = UnityEngine.SceneManagement.SceneManager.GetActiveScene().name;
-
-
-            
-            // Otherwise this is likely the calibrationscene
-
             cam = Camera.main;
-            
+            CreateLuminanceCamera();
+
+            string sceneName = UnityEngine.SceneManagement.SceneManager.GetActiveScene().name;
             string timestamp = DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss");  // e.g., 2025-10-22_14-30-00
-            string logDirectory = Path.Combine(
+            logDirectory = Path.Combine(
                 Environment.GetFolderPath(Environment.SpecialFolder.Desktop),
                 "AcroGenData",
                 subjectName,
@@ -101,33 +118,30 @@ namespace ViveSR.anipal.Eye
                 timestamp
             );
             Directory.CreateDirectory(logDirectory);
+
+            SRanipal_Eye.WrapperRegisterEyeDataCallback(Marshal.GetFunctionPointerForDelegate((SRanipal_Eye.CallbackBasic)EyeCallback));
+            UnityEngine.Debug.Log("EyeCallback registered");
+
+            if (CalibrateOnStart)
+                PupilCalibration();
+
+            
             string logPath = Path.Combine(logDirectory, "eye_tracking_log_test.csv");
 
             writer = new StreamWriter(logPath);
-            writer.WriteLine("Timestamp,PupilDiameterLeft,PupilDiameterRight,GazeLeftX,GazeLeftY,GazeLeftZ,GazeRightX,GazeRightY,GazeRightZ"); // header row
-            
-
+            writer.WriteLine("Timestamp,Luminance,PupilDiameterLeft,PupilDiameterRight,GazeLeftX,GazeLeftY,GazeLeftZ,GazeRightX,GazeRightY,GazeRightZ"); // header row
             writer.Flush();
 
-            if (luminanceEnabled)
-            {
-                CreateLuminanceCamera();
-            }
-
-            fearChecked = true;
-
-            SRanipal_Eye.WrapperRegisterEyeDataCallback(Marshal.GetFunctionPointerForDelegate((SRanipal_Eye.CallbackBasic)EyeCallback));
-            UnityEngine.Debug.Log("EyeCallback registered and logging to: " + logPath);
+            UnityEngine.Debug.Log("EyeCallback logging to: " + logPath);     
         }
 
         void OnApplicationQuit()
         {
             SRanipal_Eye_Framework.Instance.EnableEyeDataCallback = false;
-            if (writer != null)
-            {
-                writer.Flush();
-                writer.Close();
-            }
+            writer.Flush();
+            writer.Close();
+            incrementsWriter.Flush();
+            incrementsWriter.Close();
         }
 
         void Update()
@@ -135,45 +149,66 @@ namespace ViveSR.anipal.Eye
             if (SRanipal_Eye_Framework.Status != SRanipal_Eye_Framework.FrameworkStatus.WORKING &&
                 SRanipal_Eye_Framework.Status != SRanipal_Eye_Framework.FrameworkStatus.NOT_SUPPORT)
             {
+                UnityEngine.Debug.Log("Update returning...");
                 return;
             }
-
+            
             if (SRanipal_Eye_Framework.Instance.EnableEyeDataCallback && !eye_callback_registered)
             {
                 SRanipal_Eye.WrapperRegisterEyeDataCallback(Marshal.GetFunctionPointerForDelegate((SRanipal_Eye.CallbackBasic)EyeCallback));
                 UnityEngine.Debug.Log("EyeTrackingManager: EyeCallback registered (v1)");
                 eye_callback_registered = true;
             }
-
+            
+            if (!callbackReceived)
+                UnityEngine.Debug.Log("No callback yet...");
+            
             // Write any buffered data to file
-            while (logQueue.Count > 0)
+            if (calibrated)
             {
-                string line = logQueue.Dequeue();
-                writer.WriteLine(line);
+                while (logQueue.Count > 0)
+                {
+                    string line = logQueue.Dequeue();
+                    writer.WriteLine(line);
+                }
+                writer.Flush();
             }
-
-            writer.Flush();
+            else
+            {
+                while (incQueue.Count > 0)
+                {
+                    string line = incQueue.Dequeue();
+                    incrementsWriter.WriteLine(line);
+                }
+                incrementsWriter.Flush();
+            } 
         }
 
         [MonoPInvokeCallback]
         private static void EyeCallback(ref EyeData eyeData)
         {
+            callbackCount += 1;
             if (callbackCount % logEveryN != 0) return;
+            instance.callbackReceived = true;
             var timestamp = eyeData.timestamp;
             var pupilLeft = eyeData.verbose_data.left.pupil_diameter_mm;
             var pupilRight = eyeData.verbose_data.right.pupil_diameter_mm;
             var gazeLeft = eyeData.verbose_data.left.gaze_direction_normalized;
             var gazeRight = eyeData.verbose_data.right.gaze_direction_normalized;
-
-            // Format a CSV line
-            string line = $"{timestamp:F3},{pupilLeft},{pupilRight}," +
-                        $"{gazeLeft.x},{gazeLeft.y},{gazeLeft.z}," +
-                        $"{gazeRight.x},{gazeRight.y},{gazeRight.z},";
-
-            logQueue.Enqueue(line);
-
+            string line = $"{timestamp:F3},{luminance},{pupilLeft},{pupilRight}," +
+                    $"{gazeLeft.x},{gazeLeft.y},{gazeLeft.z}," +
+                    $"{gazeRight.x},{gazeRight.y},{gazeRight.z},";
+            UnityEngine.Debug.Log($"Line: {line}");
+            if (!instance.calibrated & instance.calibrating)
+            {
+                incQueue.Enqueue(line);
+            }
+            else
+            {
+                logQueue.Enqueue(line);
+            }
         }
-
+        
         private void FixedUpdate()
         {
             if (luminanceEnabled && luminanceCreated && Time.time - luminanceTime >= luminanceRate)
@@ -183,6 +218,7 @@ namespace ViveSR.anipal.Eye
                 luminanceTime = Time.time;
             }
         }
+        
 
         private float GetLuminance(RenderTexture renderTexture)
         {
@@ -203,7 +239,6 @@ namespace ViveSR.anipal.Eye
             }
 
             float averageLuminance = totalLuminance / allColors.Length;
-
 
             RenderTexture.active = oldRenderTexture;
             UnityEngine.Object.Destroy(texture2D);
@@ -226,68 +261,42 @@ namespace ViveSR.anipal.Eye
 
         public void PupilCalibration()
         {
-            fearIncrements = new List<float>();
+            increments = new List<float>();
             StartCoroutine(PupilCalibrationCo());
         }
 
         private IEnumerator PupilCalibrationCo()
         {
             UnityEngine.Debug.Log("Starting Pupil Calibration");
+            string incPath = Path.Combine(logDirectory, "increments.csv");
+            incrementsWriter = new StreamWriter(incPath);
+            UnityEngine.Debug.Log("Calibration logging to: " + incPath);    
+            incrementsWriter.WriteLine("Timestamp,Luminance,PupilDiameterLeft,PupilDiameterRight,GazeLeftX,GazeLeftY,GazeLeftZ,GazeRightX,GazeRightY,GazeRightZ"); // header row
+            incrementsWriter.Flush();
+            
             RawImage rawImage = calibrationScreen.GetComponent(typeof(RawImage)) as RawImage;
-            UnityEngine.Debug.LogFormat("Starting pupil calibration now...");
             float luminanceDelay = 10.0f;
             int colorVal = 0;
             byte colorByte = (byte)colorVal;
             //UnityMainThreadDispatcher.Instance().Enqueue(() => PupilCalibrationLog("pupil_calibration_started", 0f));
+            Canvas canvas = calibrationScreen.GetComponent<Canvas>();
+            canvas.renderMode = RenderMode.ScreenSpaceCamera; // Make sure it's Screen Space - Camera
+            canvas.worldCamera = luminanceCamera;
             calibrationScreen.SetActive(true);
+            calibrating = true;
             for (int i = 0; i < 18; i++)
             {
-                
                 colorVal = i * 15;
                 colorByte = (byte)colorVal;
                 rawImage.color = new Color32(colorByte, colorByte, colorByte, 255);
-                //UnityMainThreadDispatcher.Instance().Enqueue(() => PupilCalibrationLog(string.Format("pupil_calibration_{0}", colorVal), luminanceDelay));
                 yield return new WaitForSecondsRealtime(luminanceDelay);
-                UnityEngine.Debug.Log($"Light level: {colorVal}, diameter: {pupilDiameterLeft}.");
-                Increment(pupilDiameterLeft, pupilDiameterRight);
                 luminanceDelay = 2.0f;
             }
             calibrationScreen.SetActive(false);
             rawImage.color = new Color32(0, 0, 0, 255);
-            //UnityMainThreadDispatcher.Instance().Enqueue(() => PupilCalibrationLog("pupil_calibration_ended", 0f));
-            UpdateIncrements();
-        }
-
-        private static void Increment(float pupilDiameterLeft, float pupilDiameterRight)
-        {
-            if (fearChecked && fearEnabled)
-            {
-                fearIncrements.Add((pupilDiameterLeft + pupilDiameterRight) / 2);
-            }
-            else
-            {
-                UnityEngine.Debug.Log("Not adding increments properly...");
-            }
-
-        }
-
-        public void UpdateIncrements()
-        {
-            UnityEngine.Debug.Log("Updateing Increments");
-            string incrementsDirectory = Path.Combine(
-                Environment.GetFolderPath(Environment.SpecialFolder.Desktop),
-                "AcroGenData",
-                subjectName
-            );
-            Directory.CreateDirectory(incrementsDirectory);
-            string incPath = Path.Combine(incrementsDirectory, "increments.csv");
-
-            StreamWriter incWriter = new StreamWriter(incPath);
-            incWriter.WriteLine(string.Join(",", fearIncrements));
-            incWriter.Flush();
-            incWriter.Close();
-            UnityEngine.Debug.Log("?? Fear increments configured");
-
+            calibrated = true;
+            calibrating = false;
+            
         }
     }
 }
